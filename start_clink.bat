a794f786aa1bbd1a138d6a0828109000a7bd7214
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2024-06-29 08:24:15 +0200

    Add aliases and user aliases

--- start_clink.bat
+++ start_clink.bat
@@ -1,5 +1,18 @@
 @echo off
 
+@REM Aliases
+doskey ls      = dir /w $*
+doskey ll      = dir $*
+doskey clear   = cls
+doskey cp      = copy $*
+doskey mv      = move $*
+doskey rm      = del $*
+doskey e       = start %windir%\explorer.exe .
+
+if exist %~dp0user_aliases.bat (
+    call %~dp0user_aliases.bat
+)
+
 @REM Start clink
 set CLINK_INPUTRC=%~dp0
 set CLINK_PATH=%~dp03rdparty\clink-zoxide;%~dp03rdparty\clink-gizmos
