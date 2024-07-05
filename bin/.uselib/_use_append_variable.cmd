202b219d8dc09aca8b17f71373fcf855f53a099f
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2024-07-05 20:01:32 +0200

    Use: avoid calling defer scripts, appending path or variable multiple times

--- bin/.uselib/_use_append_variable.cmd
+++ bin/.uselib/_use_append_variable.cmd
@@ -6,6 +6,10 @@
 ::: %2 variable that we append
 
 @echo off
-setlocal enableDelayedExpansion 
+setlocal enableDelayedExpansion
 
-echo !%1!,%2
+::: Set a variable, and use the endlocal & trick to get the value out of the local block
+set __USE_TEMP_APPEND_VAR=!%1!,%2
+endlocal & set "%1=%__USE_TEMP_APPEND_VAR%"
+
+set __USE_TEMP_APPEND_VAR=
--- bin/.uselib/_use_setup_tool.cmd
+++ bin/.uselib/_use_setup_tool.cmd
@@ -29,9 +29,7 @@ for /f "delims=" %%a in ('jq "has(""%1"")" %2') do (
 ::: Setup sub-tools
 :::============================================================================
 for /f "delims=" %%a in ('jq -r "((.[""%1""].use // []) - [%__USE_TOOLS%])[]?" %2') do (
-    for /f "delims=" %%v in ('%~dp0_use_append_variable.cmd __USE_TOOLS ""%%a""') do (
-        set __USE_TOOLS=%%v
-    )
+    call %~dp0_use_append_variable.cmd __USE_TOOLS ""%%a""
     if "%%a" NEQ "" (
         call %~dp0\_use_setup_tool.cmd %%a %2
     )
@@ -71,7 +69,12 @@ for /f "delims=" %%a in ('%~dp0\_use_set_path.cmd %1 %2') do (
 :::============================================================================
 for /f "delims=" %%a in ('jq -r ".[""%1""].defer[]?" %2') do (
     if "%%a" NEQ "" (
-        call "%%a" >NUL
+        ::: Only call a script if it has not been called before
+        echo.%__USE_DEFER_SCRIPTS% | findstr /C:"%%a" >NUL
+        if errorlevel 1 (
+            call %~dp0_use_append_variable.cmd __USE_DEFER_SCRIPTS "%%a"
+            call "%%a" >NUL
+        )
     )
 )
 
