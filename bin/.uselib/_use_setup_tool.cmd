b6d99b4236447e09776fbc217fea27bc7d6b4001
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2024-07-05 19:03:07 +0200

    Use: add prepend, path and go

--- bin/.uselib/_use_setup_tool.cmd
+++ bin/.uselib/_use_setup_tool.cmd
@@ -6,6 +6,15 @@
 
 @echo off
 
+::: Follow the go directory, but only for the first tool set
+:::============================================================================
+if "%__USE_TOOLS:~2,-2%"=="%1" (
+    for /f "delims=" %%a in ('jq -r ".[""%1""].go" %2') do (
+        if "%%a" NEQ "null" (
+            cd %%a
+        )
+    )
+)
 
 ::: Check that the tool exist
 :::============================================================================
@@ -39,9 +48,23 @@ for /f "delims=" %%a in ('jq -r ".[""%1""].set | try to_entries[] | join(""="")"
 ::: Append variables
 :::============================================================================
 for /f "delims=" %%a in ('%~dp0\_use_do_append.cmd %1 %2') do (
-    if "%%a%" NEQ "" (
+    if "%%a" NEQ "" (
         set "%%a"
     )
+)
+
+::: Prepend variables
+:::============================================================================
+for /f "delims=" %%a in ('%~dp0\_use_do_prepend.cmd %1 %2') do (
+    if "%%a" NEQ "" (
+        set "%%a"
+    )
+)
+
+::: Prepend to the PATH variable
+:::============================================================================
+for /f "delims=" %%a in ('%~dp0\_use_set_path.cmd %1 %2') do (
+    set "PATH=%%a"
 )
 
 ::: Call all deferred scripts
