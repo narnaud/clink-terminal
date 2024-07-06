0ddea9f5f45a91c44aa744aed9d8908a80a6c984
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2024-07-06 21:48:24 +0200

    fzf: add history listing

--- bin/fzf-git-stash.cmd
+++ bin/fzf-git-stash.cmd
@@ -13,4 +13,4 @@ set __GIT_HELP=--header "CTRL-X (Drop stash)"
 set __GIT_PREVIEW_CMD="git show --color=always {1}"
 set __GIT_CTRL_X="ctrl-x:execute-silent(git stash drop {1})+reload(%__GIT_CMD%)"
 
-%__GIT_CMD% 2>nul | fzf  %__ZFZ_FRAME% %__ZFZ_LAYOUT% %__ZFZ_PREVIEW_WINDOW%  %__GIT_TITLE% %__GIT_HELP% -d: --preview=%__GIT_PREVIEW_CMD% --bind=%__GIT_CTRL_X%
+%__GIT_CMD% 2>nul | fzf  %__ZFZ_FRAME% %__ZFZ_LAYOUT% %__ZFZ_PREVIEW_WINDOW% %__GIT_TITLE% %__GIT_HELP% -d: --preview=%__GIT_PREVIEW_CMD% --bind=%__GIT_CTRL_X%
