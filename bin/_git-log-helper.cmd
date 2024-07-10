0f535a8edb4a803af44a8eb0d802b6ae099f5bf4
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2024-07-10 13:40:44 +0200

    fzf: Small fixes for the fzf-git-branches command

--- bin/_git-log-helper.cmd
+++ bin/_git-log-helper.cmd
@@ -1,3 +1,7 @@
 @echo off
 
-git log --graph --pretty=format:"%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset" --abbrev-commit --date=relative --color=always %*
+if "%1" == "*" (
+    git log --graph --pretty=format:"%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset" --abbrev-commit --date=relative --color=always
+) else (
+    git log --graph --pretty=format:"%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset" --abbrev-commit --date=relative --color=always %*
+)
--- bin/fzf-git-branches.cmd
+++ bin/fzf-git-branches.cmd
@@ -7,13 +7,13 @@ set __ZFZ_LAYOUT=--layout=reverse --height=50%% --min-height=20
 set __ZFZ_PREVIEW_WINDOW=--preview-window="down,50%%,border-top" --bind="ctrl-/:change-preview-window(right,50%%,border-left|hidden|)"
 
 ::: Git specific options
-set __GIT_CMD=_git-branches-helper.cmd
-set __GIT_CMD_ALL=_git-branches-helper.cmd -a
+set __GIT_CMD=git branch --sort=committerdate --sort=HEAD --format="%%(HEAD) %%(color:yellow)%%(refname:short) %%(color:green)(%%(committerdate:relative)) - %%(color:white)%%(subject)%%(color:reset)" --color=always
+set __GIT_CMD_ALL=%__GIT_CMD% -a
 set __GIT_TITLE=--border-label="🌳 Git branches"
 set __GIT_HELP=--header "CTRL-A (Show all branches) / ALT-X (Drop branch)"
 set __GIT_PREVIEW_CMD="_git-log-helper.cmd {1}"
-set __GIT_CTRL_A="ctrl-a:change-border-label(🌳 Git all branches)+reload(%__GIT_CMD_ALL%)"
-set __GIT_ALT_X="alt-x:execute-silent(git branch -D {1})+reload(%__GIT_CMD%)+reload(%__GIT_CMD%)"
+set __GIT_CTRL_A="ctrl-a:change-border-label(🌳 Git all branches)+reload(%__GIT_CMD_ALL:"="""%)"
+set __GIT_ALT_X="alt-x:execute-silent(git branch -D {1})+reload(%__GIT_CMD:"="""%)"
 
 %__GIT_CMD% 2>nul | fzf  %__ZFZ_FRAME% %__ZFZ_LAYOUT% %__ZFZ_PREVIEW_WINDOW% %__GIT_TITLE% %__GIT_HELP% --preview=%__GIT_PREVIEW_CMD% --bind=%__GIT_CTRL_A% --bind=%__GIT_ALT_X%
 
