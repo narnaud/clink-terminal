69ee61131cffea011aed7d49579ea5a8b67fc963
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2024-07-10 13:40:44 +0200

    fzf: Small fixes for the fzf-git-branches command

--- bin/_git-branches-helper.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-
-git branch --sort=committerdate --sort=HEAD --format="%%(HEAD) %%(color:yellow)%%(refname:short) %%(color:green)(%%(committerdate:relative)) - %%(color:white)%%(subject)%%(color:reset)" --color=always %*
