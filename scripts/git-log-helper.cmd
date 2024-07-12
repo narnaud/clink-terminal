90c97ce030377dde786e336a43fc1e7e32802a2e
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2024-07-12 20:42:39 +0200

    fzf: Add git branches lua function

---
+++ scripts/git-log-helper.cmd
@@ -0,0 +1,2 @@
+@echo off
+git log --graph --pretty=format:"%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset" --abbrev-commit --date=relative --color=always %*
