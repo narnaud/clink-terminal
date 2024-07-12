e018d27d338d53649c3e8501cc1ed97ef032eb51
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2024-07-12 20:57:16 +0200

    fzf: Remove batch files now that we have lua functions

--- bin/_git-log-helper.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-if "%1" == "*" (
-    git log --graph --pretty=format:"%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset" --abbrev-commit --date=relative --color=always
-) else (
-    git log --graph --pretty=format:"%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset" --abbrev-commit --date=relative --color=always %*
-)
