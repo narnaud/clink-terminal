eee37e040e8a3aeb81b3609815baa1325b85e9a1
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2025-02-07 20:59:56 +0100

    Change prompt style
    Use classic mode, 2 lines, with slant

--- scripts/flexprompt_config.lua
+++ scripts/flexprompt_config.lua
@@ -1,18 +1,24 @@
+-- WARNING:  This file gets overwritten by the 'flexprompt configure' wizard!
+--
+-- If you want to make changes, consider copying the file to
+-- 'flexprompt_config.lua' and editing that file instead.
+
 flexprompt = flexprompt or {}
 flexprompt.settings = flexprompt.settings or {}
 flexprompt.settings.charset = "unicode"
+flexprompt.settings.connection = "solid"
 flexprompt.settings.flow = "concise"
 flexprompt.settings.frame_color = "darkest"
-flexprompt.settings.heads = "round"
+flexprompt.settings.heads = "slant"
 flexprompt.settings.left_frame = "round"
-flexprompt.settings.left_prompt = "{battery:levelicon:onlyicon}{histlabel}{cwd:type=smart}{env:label=󱁤::var=USE_PROMPT:color=black}{git}"
-flexprompt.settings.lines = "one"
+flexprompt.settings.left_prompt = "{admin}{battery:levelicon:onlyicon}{histlabel}{cwd:type=smart}{env:label=󱁤::var=USE_PROMPT:color=black}{scm}"
+flexprompt.settings.lines = "two"
 flexprompt.settings.nerdfonts_version = 3
 flexprompt.settings.nerdfonts_width = 2
 flexprompt.settings.powerline_font = true
-flexprompt.settings.right_frame = "round"
+flexprompt.settings.right_frame = "none"
 flexprompt.settings.right_prompt = "{overtype}{exit}{duration}{time:format=%H:%M:%S}"
-flexprompt.settings.separators = "round"
+flexprompt.settings.separators = "slant"
 flexprompt.settings.spacing = "sparse"
 flexprompt.settings.style = "classic"
 flexprompt.settings.symbols =
@@ -23,6 +29,6 @@ flexprompt.settings.symbols =
         winterminal = "❯",
     },
 }
-flexprompt.settings.tails = "round"
+flexprompt.settings.tails = "flat"
 flexprompt.settings.use_8bit_color = true
 flexprompt.settings.use_icons = true
