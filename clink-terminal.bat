8e3ba70a326c9bc2cb27a89b18d2678cd95b0e51
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2025-03-01 10:40:27 +0100

    refactor: Format the start script

--- clink-terminal.bat
+++ clink-terminal.bat
@@ -21,14 +21,29 @@ if exist %USERPROFILE%\.alias.json (
 )
 
 :::============================================================================
+::: Add custom command path
+:::============================================================================
+set PATH=%PATH%;%~dp0bin
+
+:::============================================================================
 ::: Yazi setup
 :::============================================================================
 set YAZI_FILE_ONE=%USERPROFILE%\scoop\apps\git\current\usr\bin\file.exe
 
 :::============================================================================
-::: Add custom command path
+::: Dirx setup
 :::============================================================================
-set PATH=%PATH%;%~dp0bin
+::: Change dirx colors
+set DIRX_COLORS=di=36:di hi=37:.*=37;2:ex=32:sc=33:bu=33:cm=34;4:*.md=35:do=35
+
+:::============================================================================
+::: Fzf setup
+:::============================================================================
+::: Change the command used to list files and folders for fzf (use dirx, for icons)
+set FZF_DEFAULT_OPTS=--ansi
+set FZF_CTRL_T_COMMAND=dirx.exe /b /s /X:d /a:-s-h --bare-relative --icons=always --escape-codes=always --utf8 $dir
+set FZF_ALT_C_COMMAND=dirx.exe /b /s /X:d /a:d-s-h --bare-relative --icons=always --escape-codes=always --utf8 $dir
+set FZF_ICON_WIDTH=2
 
 :::============================================================================
 ::: Start ssh-agent
@@ -42,13 +57,6 @@ ssh-agent -s 2>nul
 set CLINK_INPUTRC=%~dp0
 ::: Add some script paths. Note that clink-flex-prompt must be before scripts
 set CLINK_PATH=%~dp03rdparty\clink-flex-prompt;%~dp03rdparty\clink-completions;%~dp03rdparty\clink-zoxide;%~dp03rdparty\clink-gizmos;%~dp0scripts
-::: Change dirx colors
-set DIRX_COLORS=di=36:di hi=37:.*=37;2:ex=32:sc=33:bu=33:cm=34;4:*.md=35:do=35
-::: Change the command used to list files and folders for fzf (use direx, for icons)
-set FZF_DEFAULT_OPTS=--ansi
-set FZF_CTRL_T_COMMAND=dirx.exe /b /s /X:d /a:-s-h --bare-relative --icons=always --escape-codes=always --utf8 $dir
-set FZF_ALT_C_COMMAND=dirx.exe /b /s /X:d /a:d-s-h --bare-relative --icons=always --escape-codes=always --utf8 $dir
-set FZF_ICON_WIDTH=2
 
 ::: Finally, inject clink
 clink inject --quiet
